8625cd2c24ab5e32b6d908b8df2b338143346804
unknown <binhk4a@gmail.com>
2022-01-12 20:22:49 +0700

    th

---
+++ src/com/BinhAn/Main.java
@@ -0,0 +1,29 @@
+package com.BinhAn;
+
+import javafx.scene.shape.Circle;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[4];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle();
+        circles[3] = new Circle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Comparator circleComparator = new CircleComparator();
+        Arrays.sort(circles, circleComparator);
+
+        System.out.println("After-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
